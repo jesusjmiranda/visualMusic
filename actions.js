521945cc80804234f763a8336528e7dad19d579f
jjmiranda8 <jmlivingfree@gmail.com>
2020-12-09 21:25:13 -0600

    logic for some classes to start and beginings of the draw piano function

--- actions.js
+++ actions.js
@@ -1,6 +1,35 @@
 let container = document.querySelector(".main");
 
-let piano = document.createElement("div");
-piano.classList.add("piano");
-container.appendChild(piano);
+//PIANO GUI
+
+
+let drawPiano = () => {
+
+    let piano = document.createElement("div");
+    piano.classList.add("piano");
+
+
+    for(let i = 0; i < 12; i++) {
+
+        let key = document.createElement("div");
+    }
+
+    container.appendChild(piano);
+}
+
+
+//DEFINING CLASSES TO CREATE CHORDS ON THE PIANO GUI
+let Chord = class {
+    constructor(name, notes= []) {
+        this.name = name;
+        this.notes = notes;
+    }
+}
+
+let Music = class {
+
+}
+
+let a = new Chord("amin", ["a", "c", "e"])
+
 

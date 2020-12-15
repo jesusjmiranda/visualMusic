d7e050b3ccd8be9148ee0ee779c6b314a97a0dc3
jjmiranda8 <jmlivingfree@gmail.com>
2020-12-14 23:39:58 -0600

    add styling for textContent and functionality on drawPiano(). Could be used for drawing the highlighted note at different octaves once higlighted at a lower octave

--- actions.js
+++ actions.js
@@ -2,45 +2,92 @@ let container = document.querySelector(".main");
 
 let selection_mode = 'normal';
 
+let activeNotes = []
+
 
 //PIANO GUI
 let drawPiano = (octaves=1) => {
-
-    let hover = (node) => {
-        node.classList.add("hover");
-    }
-
-    let stopHover = (node) => {
-        node.classList.remove("hover");
-    }
 
     let piano = document.createElement("div");
     piano.classList.add("piano");
 
     for(let i = 0; i < 12 * octaves; i++) {
         let key = document.createElement("div");
-        key.addEventListener("mouseenter", hover(key));
-        key.addEventListener("mouseleave", stopHover(key));
-        
+        key.addEventListener("mouseenter", function() {
+            key.classList.add("hover")
+        });
+        key.addEventListener("mouseleave", function() {
+            key.classList.remove("hover");
+        });
         key.addEventListener("click", function() { 
             let classes = key.className.split(" ");
             if(!classes.includes("clicked")) {
                 key.classList.add("clicked")
+                activeNotes.push("something");
             } else {
                 key.classList.remove("clicked");
             }
         });
+
+        switch(i){
+            case 0:
+                key.textContent = "c";
+                break;
+            case 1:
+                key.textContent = "c/d";
+                break;
+            case 2:
+                key.textContent = "d";
+                break;
+            case 3:
+                key.textContent = "d/e";
+                break;
+            case 4:
+                key.textContent = "e";
+                break;
+            case 5:
+                key.textContent = "f";
+                break;
+            case 6:
+                key.textContent = "f/g";
+                break;
+            case 7:
+                key.textContent = "g";
+                break;
+            case 8:
+                key.textContent = "g/a";
+                break;
+            case 9:
+                key.textContent = "a";
+                break;
+            case 10:
+                key.textContent = "a/b";
+                break;
+            case 11:
+                key.textContent = "b";
+                break;
+        }
 
         if (i == 0 || i == 2 || i == 4 || i == 5 || i == 7 || i == 9 || i == 11) {
             key.classList.add("white-key"); 
         } else {
             key.classList.add("black-key");
         }
+
         piano.appendChild(key)
     }
 
     container.appendChild(piano);
 }
+
+//some resetColor() function to call when a key is pressed to activate selection
+//to reset the colors so that the other octaves recieve the change
+
+
+
+
+
+
 
 
 /*
@@ -60,9 +107,9 @@ let Music = class {
 }
 */
 drawPiano()
-drawPiano()
-drawPiano()
-
 //might need a label notes function that gets called within the drawPiano function
 //that 
 
+function something(){
+    console.log(activeNotes)
+}

93315a59fc581625b56796ebf8d07b3dd8730cb6
jjmiranda8 <jmlivingfree@gmail.com>
2020-12-20 18:08:55 -0600

    added function for getting scale notes in array.

--- actions.js
+++ actions.js
@@ -4,6 +4,90 @@ let selection_mode = 'normal';
 
 let activeNotes = []
 
+let noteNums = {
+    c:     0,
+    "c/d": 1,
+    d:     2,
+    "d/e": 3,
+    e:     4,
+    f:     5,
+    "f/g": 6,
+    g:     7,
+    "g/a": 8,
+    a:     9,
+    "a/b": 10,
+    b:     11
+}
+
+let noteNames = {
+    0:    "c",
+    1:  "c/d",
+    2:    "d",
+    3:  "d/e",
+    4:    "e",
+    5:    "f",
+    6:  "f/g", 
+    7:    "g",
+    8:  "g/a",
+    9:    "a",
+    10: "a/b",
+    11:   "b"
+}
+let possibleNotes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+//the w stands for whole (as in whole-step)
+//the h stands for half (as in half-steps)
+
+let modeIntervals = {
+    "major":    "wwhwwwh",
+    "dorian":   "whwwwhw",
+    "phrygian": "hwwwhww",
+    "lydian":   "wwwhwwh",
+    "m-lydian": "wwhwwhw",
+    "aeolian":  "whwwhww",
+    "locrian":  "hwwhwww"
+}
+
+let scale = (note, mode="major") => {
+
+  console.log(`${note} ${mode}`);
+  let result = [];
+  let pattern = (modeIntervals[mode]).split("");
+  let currentScaleDegree = 0;
+  result.push(noteNums[note])
+  let lastPushed = noteNums[note];
+
+  pattern.forEach(step => {
+      if (step == 'w') {
+        let toPush = lastPushed + 2;
+        if (toPush > 11) {
+          toPush -= 12;
+        } 
+        result.push(toPush);
+        lastPushed = toPush;
+      } else  if (step == 'h') {
+        let toPush = lastPushed + 1;
+        if (toPush > 11) {
+          toPush -= 12;
+        }
+        result.push(toPush);
+        lastPushed = toPush;
+      }
+  });
+
+  console.log(something)
+  return result; 
+}
+
+//translates between number and name of note
+let translate = (array, typeDesired) => {
+    let result = [];
+    arrayOfNoteNums.forEach(element => {
+
+    });
+    return result;
+}
+
 //PIANO GUI
 let drawPiano = (octaves=1) => {
     let piano = document.createElement("div");
@@ -83,7 +167,7 @@ let drawPiano = (octaves=1) => {
 
 //Chord storage 
 //container that will display my saved chords
-let numberOfChords = 0;
+//let numberOfChords = 0;
 
 //some resetColor() function to call when a key is pressed to activate selection
 //to reset the colors so that the other octaves recieve the change
@@ -93,23 +177,8 @@ let numberOfChords = 0;
 /*
 //SELECTION MODES
 
-
-//DEFINING CLASSES TO CREATE CHORDS ON THE PIANO GUI
-let Chord = class {
-    constructor(name, notes= []) {
-        this.name = name;
-        this.notes = notes;
-    }
-}
-
-let Music = class {
-
-}
 */
 drawPiano()
-//might need a label notes function that gets called within the drawPiano function
-//that 
-//
 
 let something = () => {
     console.log(activeNotes);
@@ -136,6 +205,7 @@ let reset = () => {
 
     drawPiano()
 }
+
 
 let resetButton = document.createElement("div");
 resetButton.classList.add("resetButton");

1d5aa355bf2cfc17b841454c6d30f5ce59a69509
jjmiranda8 <jmlivingfree@gmail.com>
2020-12-29 16:57:32 -0600

    add drawPiano() function without the event listeners for clicks. Needs styling

--- main.js
+++ main.js
@@ -94,10 +94,68 @@ var Chord = /** @class */ (function () {
     return Chord;
 }());
 ;
+var mainContent = document.querySelector('.main');
 var drawPiano = function () {
-    var mainContent = document.querySelector('.main');
     var piano = document.createElement('div');
     piano.classList.add('piano');
     mainContent.appendChild(piano);
+    var whiteKeys = document.createElement('div');
+    var blackKeys = document.createElement('div');
+    whiteKeys.classList.add('whiteKeys');
+    blackKeys.classList.add('blackKeys');
+    piano.appendChild(blackKeys);
+    piano.appendChild(whiteKeys);
+    for (var i = 0; i <= 12; i++) {
+        var key = document.createElement("div");
+        switch (i) {
+            case 0:
+                key.textContent = "c";
+                break;
+            case 1:
+                key.textContent = "c/d";
+                break;
+            case 2:
+                key.textContent = "d";
+                break;
+            case 3:
+                key.textContent = "d/e";
+                break;
+            case 4:
+                key.textContent = "e";
+                break;
+            case 5:
+                key.textContent = "f";
+                break;
+            case 6:
+                key.textContent = "f/g";
+                break;
+            case 7:
+                key.textContent = "g";
+                break;
+            case 8:
+                key.textContent = "g/a";
+                break;
+            case 9:
+                key.textContent = "a";
+                break;
+            case 10:
+                key.textContent = "a/b";
+                break;
+            case 11:
+                key.textContent = "b";
+                break;
+            case 12:
+                key.textContent = "c";
+                break;
+        }
+        if (i == 0 || i == 2 || i == 4 || i == 5 || i == 7 || i == 9 || i == 11 || i == 12) {
+            key.classList.add('whiteKey');
+            whiteKeys.appendChild(key);
+        }
+        else {
+            key.classList.add('blackKey');
+            blackKeys.appendChild(key);
+        }
+    }
 };
 drawPiano();
